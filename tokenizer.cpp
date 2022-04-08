2eca7365c64c6ee26e34159415799e3fb7c8d01f
Jonny <faane784@gmail.com>
2022-04-08 10:07:03 -0700

    Added tokenizer class.

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,1 +1,39 @@
 #include "tokenizer.h"
+#include <algorithm>
+#include <sstream>
+
+const static std::regex chars_to_keep("[^ A-z0-9.?!,]");
+const static std::regex chars_add_space("([.?!,])");
+
+int Tokenizer::create_vocab(std::vector<std::string> data)
+{ 
+        for (auto iter = data.begin(); iter != data.end();) {
+                std::string s = *iter;
+
+
+        }
+        return 0;
+}
+
+std::string Tokenizer::standardize(std::string& str)
+{
+        str = std::regex_replace(str, chars_to_keep, "");
+        str = std::regex_replace(str, chars_add_space, " \1 ");
+        std::string::iterator end = std::remove(str.end(), str.end(), ' ');
+        str.erase(end, str.end());
+        
+        return str;
+}
+
+std::vector<std::string> Tokenizer::split(const std::string& str, char delim)
+{
+        std::vector<std::string> split_str;
+        std::stringstream ss(str);
+        std::string substr;
+
+        while (getline(ss, substr, delim)) {
+                split_str.push_back(substr);
+        }
+
+        return split_str;
+}
--- tokenizer.h
+++ tokenizer.h
@@ -1,8 +1,24 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 
+#include <regex>
+#include <set>
+#include <string>
+#include <vector>
+
 class Tokenizer
 {
+public:
+        int create_vocab(std::vector<std::string> data);
+private:
+        struct Token {
+                std::string string;
+                int count = 0;
+                bool operator<(Token& rhs) {return this->count < rhs.count; }
+        };
+        std::set<Token> _tokens;
+        std::string standardize(std::string& str);
+        std::vector<std::string> split(const std::string& str, char delim);
 };
 
 #endif // TOKENIZER_H
