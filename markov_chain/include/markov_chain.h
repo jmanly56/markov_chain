83d688b68c030d80c471a8cade8acc5cf240bf22
Jonny <faane784@gmail.com>
2022-04-19 22:25:51 -0700

    Setup discrete distribution in each node.

--- markov_chain/include/markov_chain.h
+++ markov_chain/include/markov_chain.h
@@ -5,7 +5,6 @@
 #include <vector>
 #include <map>
 #include <random>
-#include "node.h"
 
 /// @brief A simple typedef used by the vectorizer. A vector of vectors of ints.
 typedef std::vector<std::vector<int32_t>> ragged_matrix_t;
@@ -15,16 +14,19 @@ public:
         Markov_Chain();
         int create_chain(const ragged_matrix_t& data, size_t vocab_size);
 private:
-        typedef struct {
+        typedef struct Node{
                 int32_t id = 0;
                 std::vector<int32_t> next_ids;
-                std::vector<float> probs;
+                std::vector<int32_t> probs;
+                std::discrete_distribution<int32_t> d;
         } node_t;
 
         static std::default_random_engine _rand_gen;
-        static std::uniform_real_distribution<float> uniform;
         std::vector<node_t> _nodes;
+
         int _process_id(const int32_t node_id, const int32_t prev_node_id);
-        void _process_line(const std::vector<int32_t> &line) noexcept;
+        void _process_line(const std::vector<int32_t> &line);
+        int _find_index(const std::vector<int32_t> &next_ids, int32_t id) const noexcept;
+        void _finalize_nodes() noexcept;
 };
 #endif // MARKOV_CHAIN_H
