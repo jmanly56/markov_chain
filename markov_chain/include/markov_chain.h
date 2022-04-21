1d305c070a586ee9e5720166382d15e1f274be31
Jonny <faane784@gmail.com>
2022-04-20 21:06:34 -0700

    Functioning markov chain

--- markov_chain/include/markov_chain.h
+++ markov_chain/include/markov_chain.h
@@ -13,7 +13,10 @@ class Markov_Chain {
 public:
         Markov_Chain();
         int create_chain(const ragged_matrix_t& data, size_t vocab_size);
-private:
+        std::vector<int32_t> traverse(int max_len=50) noexcept;
+        static std::string to_string(const std::vector<int32_t> &indices, const std::vector<std::string *> &vocab);
+
+    private:
         typedef struct Node{
                 int32_t id = 0;
                 std::vector<int32_t> next_ids;
@@ -21,7 +24,7 @@ private:
                 std::discrete_distribution<int32_t> d;
         } node_t;
 
-        static std::default_random_engine _rand_gen;
+        std::default_random_engine _rand_gen;
         std::vector<node_t> _nodes;
 
         int _process_id(const int32_t node_id, const int32_t prev_node_id);
