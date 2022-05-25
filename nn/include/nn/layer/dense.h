f6b869c570e0383880ea048ea5a6733e56b12798
Jonathan <jmanly784@gmail.com>
2022-05-24 22:18:39 -0700

    making the dense class.

--- nn/include/nn/layer/dense.h
+++ nn/include/nn/layer/dense.h
@@ -2,10 +2,27 @@
 #define DENSE_H
 
 #include "layer.h"
+#include "nn/activation/activation.h"
+#include <Eigen/src/Core/Matrix.h>
 
-class Dense : public Layer {
+class Dense : public Layer
+{
     public:
-    private: 
+        Dense(int units, int batch_size, std::string name = "dense",
+              Activation *activation = nullptr);
+        ~Dense();
+        void build(int input_size);
+        Eigen::MatrixXd call(Eigen::MatrixXd &m) const;
+        Eigen::MatrixXd operator()(Eigen::MatrixXd &m) const;
+        const Activation *activation() const;
+
+    private:
+        bool _built;
+        int _units;
+        int _input_size;
+        int _batch_size;
+        Eigen::MatrixXd *_weights;
+        Eigen::VectorXd *_biases;
 };
 
 #endif // DENSE_H
--- nn/include/nn/layer/layer.h
+++ nn/include/nn/layer/layer.h
@@ -1,20 +1,22 @@
 #ifndef LAYER_H
 #define LAYER_H
 
-#include <Eigen/Dense>
 #include "nn/activation/activation.h"
+#include <Eigen/Dense>
 
+class Layer
+{
+    public:
+        Layer(std::string name);
+        virtual ~Layer() = 0;
+        virtual Eigen::MatrixXd call(Eigen::MatrixXd &m) const = 0;
+        virtual Eigen::MatrixXd operator()(Eigen::MatrixXd &m) const = 0;
+        virtual const Activation *activation() const = 0;
+        const std::string &name() const;
 
-class Layer {
-        public:
-                Layer(std::string name);
-                virtual ~Layer() = 0;
-                virtual Eigen::MatrixXd call(Eigen::MatrixXd &m) = 0;
-                virtual Eigen::MatrixXd operator()(Eigen::MatrixXd &m) = 0;
-                const std::string& name() const;
-        protected:
-                std::string _name;
-                Activation *_activation;
+    protected:
+        std::string _name;
+        Activation *_activation;
 };
 
 #endif // LAYER_H
